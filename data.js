e524f320959d456aa40b7bfd4ada72df4cb4828c
sonnt74 <sonnt74@gmail.com>
2026-09-15 17:36:54 +0700

    Update data.js

--- data.js
+++ data.js
@@ -476,3 +476,6 @@ function renderColoredRouteOnMap() {
 
   showToast("Đã hiển thị xong đường tuyến phân màu và các điểm mốc!", "success");
 }
+function veLaiTuyenAB() {
+  renderColoredRouteOnMap();
+}
