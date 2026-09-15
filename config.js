27d3be12294b2a80bc50fe9cb395a695f255f922
sonnt74 <sonnt74@gmail.com>
2026-09-15 08:17:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,24 @@
+// config.js - Cấu hình kết nối và biến toàn cục
+const SUPABASE_URL = 'https://clddwitzwuewwxawuorv.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_U3tMbsj5oQ9Wub1UAJO5Cw_NXt6Px8E';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+var currentUser = { isLoggedIn: false, role: 'member', idDai: null, idTram: null, canEditMap: false };
+var map = null;
+
+var markersLayer = L.markerClusterGroup({ maxClusterRadius: 40 });
+var mxLayer = L.layerGroup();
+var polylinesLayer = L.layerGroup();
+var userLocationLayer = L.layerGroup();
+var measureLayer = L.layerGroup();
+var foundMarkerLayer = null;
+
+var isMeasuring = false;
+var measurePoints = [];
+
+var rawDaiList = [];
+var rawTramList = [];
+var rawTuyenList = [];
+var rawDoanCapList = [];
+var rawLoaiDiemList = [];
+var globalDataPoints = [];
